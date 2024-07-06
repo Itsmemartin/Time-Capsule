9895a678aab7bdd8007907f8237b95c5763ceaab
Itsmemartin_ <107768650+Itsmemartin@users.noreply.github.com>
2024-07-06 14:02:48 +0530

    Update Main.py
    Redid code to make it more user friendly

--- Main.py
+++ Main.py
@@ -9,19 +9,28 @@ client = discord.Client(command_prefix='',intents=discord.Intents.default())
 
 @client.event
 async def on_ready():
-    await client.change_presence(activity=discord.Game(name="<write anything here>"))
-    send_message.start()
+    await client.change_presence(activity=discord.Game(name="Opening Capsules..."))
     print('logged in as {0.user}'.format(client))
 
+@client.tree.command(name='Setup', description="Set time and message for the time capsule")
+async def caliberate_id(interaction: discord.Interaction, time_in_days: int, message: str, count_channel:str, message_channel: str):
+    Global time_in_days, message, count_channel, message_channel
+    time_in_days = time_in_days
+    message = message
+    count_channel = count_channel
+    message_channel = message_channel
+
+    await interaction.response.send_message(f"The Capsule will open in {time_in_days} days in {message_channel}")
+    send_message.start()
 
 @tasks.loop(seconds = 1)
 async def send_message():
   x = 0
-  for i in range(1,<after how long do you want it to send the messages, in seconds>):
+  for i in range(1, time_in_days*24*3600):
     x = x + 1
     time.sleep(1)
-    await client.get_channel(<enter channel id of channel you want the bot to count in>).send(x)
-  await client.get_channel(<channel id of channel you want it to send the message in>).send(<what you want to be sent at the end of the given time>)
+    await client.get_channel(count_channel).send(x)
+  await client.get_channel(message_channel).send(message)
 
 keep_alive.keep_alive()
 client.run(token)
